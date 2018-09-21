18681451d0661d7ee6ca48719641a87f009c7e4f
Voltura5 <rigvedmanoj1998@gmail.com>
2018-09-21 14:22:45 +0530

    add user move

--- trial1.py
+++ trial1.py
@@ -9,16 +9,22 @@ Flag = True
 while (True):
     legal_moves = board.legal_moves
     k = []
+    
     for move in legal_moves:
         k.append(move)
 
-    l = len(k)
-    if l == 0 or board.is_variant_draw() == True:
-        break
-    rand = randint(0,l-1)
-    move = k[rand]
-    board.push(move)
-    print(move)
+    if Flag == False:
+    	mymove=raw_input()
+    	board.push_san(mymove)
+    else:
+    	l = len(k)
+    	if l == 0 or board.is_variant_draw() == True:
+        	break
+    	rand = randint(0,l-1)
+    	move = k[rand]
+    	board.push(move)
+    	print(move)
+
     if Flag == True:
         Flag = False
     else:
@@ -26,7 +32,6 @@ while (True):
     
     
     print(board)
-    input()
 
 print("GAME_OVER")
 print(board.result())
